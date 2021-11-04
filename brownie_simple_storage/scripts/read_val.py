03789a8a2e5cdf35324b0cfb5ab7a47d52508436
clinthof <73394309+clinthof@users.noreply.github.com>
2021-11-04 16:52:03 -0400

    add read_val.py; JSONs from generation

--- brownie_simple_storage/scripts/read_val.py
+++ brownie_simple_storage/scripts/read_val.py
@@ -3,7 +3,10 @@ from brownie import SimpleStorage, accounts, config
 
 
 def read_contract():
-    pass
+    # always retrieves most recent deployment (address saved in build > 
+    # deployments, ABI saved in build > contracts > respective JSON by brownie)
+    simple_storage = SimpleStorage[-1]
+    simple_storage.retrieve()
 
 
 def main():
